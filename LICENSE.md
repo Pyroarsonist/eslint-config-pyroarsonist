16e04afadb608b5f3291089b02703d3583a120df
Pyroarsonist <velikiydan@gmail.com>
2026-01-07 15:12:00 +0200

    migrated to eslint flat configuration

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) [2022] [Pyroarsonist]
+Copyright (c) [2026] [Pyroarsonist]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ eslint.config.js
@@ -0,0 +1,56 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import perfectionist from 'eslint-plugin-perfectionist';
+import unicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export const recommended = [
+  js.configs.recommended,
+  unicorn.configs['flat/recommended'],
+  perfectionist.configs['recommended-natural'],
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    name: 'eslint-config-pyroarsonist/js-recommended',
+    rules: {
+      'arrow-body-style': ['error', 'as-needed'],
+      curly: ['error', 'multi-line'],
+      'import/no-dynamic-require': 'off',
+      'import/prefer-default-export': 'off',
+      'no-await-in-loop': 'off',
+      'no-console': ['error', { allow: ['info', 'error', 'warn', 'trace', 'table'] }],
+      'no-continue': 'off',
+      'no-plusplus': 'off',
+      'no-underscore-dangle': 'off',
+      'unicorn/no-anonymous-default-export': 'off',
+      'perfectionist/sort-objects': 'off',
+    },
+  },
+  prettier,
+];
+
+export const typescript = tseslint.config(
+  ...recommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+      },
+    },
+    name: 'eslint-config-pyroarsonist/ts-recommended',
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+  prettier,
+);
+
+export default {
+  configs: {
+    recommended,
+    typescript,
+  },
+};
